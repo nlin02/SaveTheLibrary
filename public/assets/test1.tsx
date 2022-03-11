6f9518048bc4d30d964a48c52b40ce4b3cb4eefe
LindaLor028 <77794383+LindaLor028@users.noreply.github.com>
2022-03-10 19:12:25 -0600

    Added levels.

---
+++ public/assets/test1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.2" name="test1" tilewidth="8" tileheight="8" tilecount="475" columns="19">
+ <image source="../../../../PhaserTest/public/8x8Platformer/AllTiles.png" width="152" height="200"/>
+</tileset>
---
+++ public/assets/test2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.2" name="test2" tilewidth="8" tileheight="8" tilecount="770" columns="22">
+ <image source="../../../../PhaserTest/public/8x8Platformer/AllTilesAndEnemies.png" width="176" height="280"/>
+</tileset>
