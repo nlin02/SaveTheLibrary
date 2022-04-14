6fc4f9faa41ff3f584852d45e379cfbda0d51e35
Katie McCarthy <kmccart3@macalester.edu>
2022-04-14 18:39:36 -0500

    renaming the screens

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,9 @@ import Phaser from 'phaser'
 import Game from './scenes/Game'
 import Game2 from './scenes/Game2'
 import UI from './scenes/UI'
-import GameOver from './scenes/GameOver'
-import TitleScene from './scenes/TitleScene'
-import Win from './scenes/Win'
+import GameOver from './screens/GameOverScreen'
+import TitleScene from './screens/HomeScreen'
+import Win from './screens/WinScreen'
 
 var config = {
 	type: Phaser.AUTO,
