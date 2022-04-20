8f51aa521f73cfbd2072ec5dac5135a8ce335da2
beambautista <84446151+beambautista@users.noreply.github.com>
2022-04-19 21:38:53 -0500

    fixed broken restart game button on game over screen

--- src/screens/GameOverScreen.ts
+++ src/screens/GameOverScreen.ts
@@ -16,7 +16,7 @@ export default class GameOver extends Phaser.Scene{
         const button = this.add.rectangle(width * 0.5, height *0.55, 150, 75,0xffffff)
             .setInteractive()
             .on(Phaser.Input.Events.GAMEOBJECT_POINTER_UP, () => {
-                this.scene.start('game')
+                this.scene.start('LevelAlexandria')
             })
 
         this.add.text(button.x, button.y, 'Play Again',{
