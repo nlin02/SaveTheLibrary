554a8117b4fca8e3e5d9e345a179bf7f5546610e
beambautista <84446151+beambautista@users.noreply.github.com>
2022-04-25 01:45:14 -0500

    Added custom font and background for gameover

--- src/screens/GameOverScreen.ts
+++ src/screens/GameOverScreen.ts
@@ -1,19 +1,36 @@
 import Phaser from 'phaser'
+import WebFontFile from '../WebFontFile'
 
 export default class GameOver extends Phaser.Scene{
     constructor(){
         super('game-over')
     }
 
+    preload() {
+        this.load.addFile(new WebFontFile(this.load, [
+            'Redressed',
+            'Livvic',
+            'Roboto'
+        ]))
+        this.load.image('deathBackground', 'assets/GameOverBackground.png')
+
+    }
+
     create(){
+        const background = this.add.image(0,0,"deathBackground")
+        background.setScale(1.1,1.1)
+        background.setOrigin(0,0)
+
         const{width, height} = this.scale
-        this.add.text(width*0.5, height *0.3, 'You failed your mission!', {
-            fontSize: '52px',
-            color: '#ff0000'
+        this.add.text(width*0.5, height *0.4, 'MISSION\n FAILED.', {
+            fontSize: '80px',
+            color: '#000000'
         })
         .setOrigin(0.5)
+        .setFontFamily("Redressed")
+        .setShadow(3,4,"#882a2a", 4)
 
-        const button = this.add.rectangle(width * 0.5, height *0.55, 150, 75,0xffffff)
+        const button = this.add.rectangle(width * 0.5, height *0.65, 150, 75,0xffffff)
             .setInteractive()
             .on(Phaser.Input.Events.GAMEOBJECT_POINTER_UP, () => {
                 this.scene.start('LevelHouse')
@@ -23,5 +40,6 @@ export default class GameOver extends Phaser.Scene{
             color: '#000000'
         })
         .setOrigin(0.5)
+        .setFontFamily("Livvic")
     }
 }
