6104fd7a7835eb3ed816ad626c7432dc60e113be
LindaLor028 <77794383+LindaLor028@users.noreply.github.com>
2022-04-30 19:31:20 -0500

    Fixed Hit Box and Star Bugs

--- src/levels/LevelAlexandria.ts
+++ src/levels/LevelAlexandria.ts
@@ -5,7 +5,8 @@ export default class LevelAlexandria extends Game {
 
     constructor() {
         let tilemapKey = "LevelAlexandria"
-        let tilemapJSONFileLocation = "assets/tilemaps/AlexandriaMap.json"
+        // let tilemapJSONFileLocation = "assets/tilemaps/AlexandriaMap.json"
+        let tilemapJSONFileLocation = "assets/tilemaps/NewAlexandriaMap.json"
         let levelTime = 800
     
         super(tilemapKey, tilemapJSONFileLocation, levelTime)
