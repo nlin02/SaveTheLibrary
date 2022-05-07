f0e4d55f941a59b0d90c2754903af111c6127da5
LindaLor028 <77794383+LindaLor028@users.noreply.github.com>
2022-05-06 23:46:08 -0500

    Updated Sound Removal code

--- src/screens/GameOverScreen.ts
+++ src/screens/GameOverScreen.ts
@@ -17,7 +17,7 @@ export default class GameOver extends Phaser.Scene{
     }
 
     create() {
-        this.sound.removeAll
+        this.sound.removeAll()
 
         const background = this.add.image(0,0,"deathBackground")
         background.setScale(1.1,1.1)
